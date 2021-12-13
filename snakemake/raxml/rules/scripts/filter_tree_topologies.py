fd45cb04165a6d8e1894fa005667ddb73f5b8e64
Julia <julaschmid@googlemail.com>
2021-12-13 22:10:13 +0100

    refactor rfdist cluster: use raxml for clustering

--- snakemake/raxml/rules/scripts/filter_tree_topologies.py
+++ snakemake/raxml/rules/scripts/filter_tree_topologies.py
@@ -8,6 +8,7 @@ eval_trees = read_file_contents(snakemake.input.all_trees)
 filter_tree_topologies(
     raxml_command=snakemake.params.raxml_command,
     eval_trees=eval_trees,
+    log_path=snakemake.output.raxml_rfdist_log,
     filtered_trees_path=snakemake.output.filtered_trees,
     clusters_path=snakemake.output.clusters,
 )
